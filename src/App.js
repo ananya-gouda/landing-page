f996d0f40f94883a607f6077f43590f80307f868
Ananya G Gouda <114345744+ananya-gouda@users.noreply.github.com>
2023-11-16 18:05:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
     <Navbar />
     <Routes>
-         <Route exact path="/" element={<Home />} />
+         <Route exact path="/home" element={<Home />} />
          <Route exact path="/about" element={<About />} />
          <Route exact path="/menu" element={<Menu />} />
          <Route exact path="/contact" element={<Contact />} />
